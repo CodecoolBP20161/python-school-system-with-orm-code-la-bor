a0890ec58591b578f676d5db9b96ee114a8b0f82
berloc <berloc1987@gmail.com>
2016-07-27 16:44:18 +0200

    finished menu

--- menu.py
+++ menu.py
@@ -22,7 +22,7 @@ class Menu:
 
     @staticmethod
     def main_menu():
-        print("\tMain menu:\n", "\t\t1: Admin\n", "\t\t2: Applicant\n", "\t\t3: Mentor\n", "\t\tPress Q to EXIT")
+        print("\tMain menu:\n", "\t\t1: Admin\n", "\t\t2: Applicant\n", "\t\t3: Mentor\n", "\t\tPress Q to EXIT", "\n")
 
     @staticmethod
     def main():
@@ -36,47 +36,51 @@ class Menu:
     @staticmethod
     def applicant_menu():
         print("\tApplicant menu:\n", "\t\t1: Application details\n", "\t\t2: Interview details\n", "\t\t3: Questions\n"
-              "\t\tq: Back to main menu:\n")
+              "\t\tPress Q to main menu:\n")
 
 
     @staticmethod
     def choose_applicant_menu():
-        option = input("Please enter the key of the option, that you want to choose: ")
-        if option == "1":
-            print("application details")
-            print('\n')
-        elif option == "2":
-            print("interview details")
-            print('\n')
-        elif option == "3":
-            print("question?")
-            print('\n')
-        elif option == "q":
-            exit()
-        else:
-            raise KeyError("There is no such option.")
+        while True:
+            option = input("Please enter the key of the option, that you want to choose: ")
+            if option == "1":
+                print("application details")
+                print('\n')
+            elif option == "2":
+                print("interview details")
+                print('\n')
+            elif option == "3":
+                print("question?")
+                print('\n')
+            elif option == "q":
+                break
+            else:
+                raise KeyError("There is no such option.")
+            Menu.applicant_menu()
 
     @staticmethod
     def admin_menu():
         print("\tAdmin menu:\n", "\t\t1: Applicants\n", "\t\t2: Interviews \n", "\t\t3: Questions \n",
-              "\t\tq: Back to main menu:\n")
+              "\t\tPress Q to main menu:\n")
 
     @staticmethod
     def choose_admin_menu():
-        option = input("Please enter the key of the option, that you want to choose: ")
-        if option == "1":
-            print("application details")
-            print('\n')
-        elif option == "2":
-            print("interview details")
-            print('\n')
-        elif option == "3":
-            print("question?")
-            print('\n')
-        elif option == "q":
-            exit()
-        else:
-            raise KeyError("There is no such option.")
+        while True:
+            option = input("Please enter the key of the option, that you want to choose: ")
+            if option == "1":
+                print("application details")
+                print('\n')
+            elif option == "2":
+                print("interview details")
+                print('\n')
+            elif option == "3":
+                print("question?")
+                print('\n')
+            elif option == "q":
+                break
+            else:
+                raise KeyError("There is no such option.")
+            Menu.admin_menu()
 
     @staticmethod
     def mentor_menu():
@@ -84,16 +88,19 @@ class Menu:
 
     @staticmethod
     def choose_mentor_menu():
-        option = input("Please enter the key of the option, that you want to choose: ")
-        if option == "1":
-            print("interview")
-        elif option == "2":
-            print("question?")
-        elif option == "q":
-            exit()
-        else:
-            raise KeyError("There is no such option.")
-
+        while True:
+            option = input("Please enter the key of the option, that you want to choose: ")
+            if option == "1":
+                print("interview")
+                print('\n')
+            elif option == "2":
+                print("question?")
+                print('\n')
+            elif option == "q":
+                break
+            else:
+                raise KeyError("There is no such option.")
+            Menu.mentor_menu()
 
 if __name__ == '__main__':
     Menu.main()
