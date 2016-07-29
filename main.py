78ee8d8db9b111452d3d1c7bf094d33aacf12022
berloc <berloc1987@gmail.com>
2016-07-29 10:51:37 +0200

    create main.py

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 from models import *
+from menu import *
+from build import *
+from example_data import *
 
-# Write here your console application
+
+
+
+input("Create database: ")
+create_table()
+input("Database imaging is in progress ⌛ ")
+generate_data()
+
+input("Hunting new applicants ☢ ")
+Applicant.detect_new_applicants()
+input("Applicant.assign_app_code_to_new_applicants")
+Applicant.assign_app_code_to_new_applicants()
+input("Applicant.school_to_applicant" + "\n")
+Applicant.school_to_applicant()
+
+input("Press enter to the main menu")
+Menu.main()
+# Applicant.check_interview_date()
+
+
