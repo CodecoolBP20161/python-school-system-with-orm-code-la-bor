9ce0a05a0e066ceecf9fb57eef044a61813848ab
lorszil <lorszil@gmail.com>
2016-08-23 15:45:35 +0200

    filter school with error is working

--- print_table.py
+++ print_table.py
@@ -5,13 +5,13 @@ class PrintTable:
 
     @staticmethod
     def print_all(query):
-        all_applicants = [applicant for applicant in query]
-        table = []
-        for applicant in all_applicants:
-            table.extend([[applicant.app_code, applicant.first_name, applicant.last_name, applicant.hometown,
-            applicant.school.name, applicant.status, applicant.email]])
-        headers = ["App_code", "First name", "Last name", "Hometown", "School", "Status", "E-mail"]
-        print(tabulate(table, headers, tablefmt="fancy_grid"))
-
+        if query:
+            all_applicants = [applicant for applicant in query]
+            table = []
+            for applicant in all_applicants:
+                table.extend([[applicant.app_code, applicant.first_name, applicant.last_name, applicant.hometown,
+                applicant.school.name, applicant.status, applicant.email]])
+            headers = ["App_code", "First name", "Last name", "Hometown", "School", "Status", "E-mail"]
+            print(tabulate(table, headers, tablefmt="fancy_grid"))
 
 # PrintTable.print_all()
