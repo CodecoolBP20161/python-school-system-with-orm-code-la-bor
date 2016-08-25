36cdf0d3194024baad46b728076203dde9f8b44f
lorszil <lorszil@gmail.com>
2016-08-25 15:30:49 +0200

    sending interview details for debugging help

--- project_email.py
+++ project_email.py
@@ -9,3 +9,21 @@ class ProjectEmail:
         subject = "Your application details"
         to_address = applicant.email
         EmailSender.email_send(to_address, body, subject)
+
+    @staticmethod
+    def send_interview_details(applicant):
+        body = "Dear {0}, \n ",
+        "Your interview details: \n",
+        "Your will start: {1} \n",
+        "Your will end: {2} \n",
+        "Your mentor will be: {3} \n",
+        "The location of the school: {4}".format(
+            applicant.first_name + ' ' + applicant.last_name,
+            applicant.interview.start_date,
+            applicant.interview.end_date,
+            applicant.interview.mentor,
+            applicant.school.location
+            )
+        subject = "Your interview details"
+        to_address = applicant.email
+        EmailSender.email_send(to_address, body, subject)
