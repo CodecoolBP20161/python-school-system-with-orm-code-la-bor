8db09334662bd297cd95bd525f8d594a7e8fba89
eszterkaloz <eszterkaloz@gmail.com>
2016-08-25 11:57:54 +0200

    Read login data from json file

--- email_model.py
+++ email_model.py
@@ -2,17 +2,22 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from models import *
-
+import json
 
 class EmailSender:
-    user = 'codelabor1@gmail.com'
-    password = 'Codelabor1234'
-    to_address = 'codelabor1@gmail.com'
-    body = "\r\n".join([
-      "Why, oh why",
-      "Why, oh why",
-      "Why, oh why"
-      ])
+
+    with open("login.json") as login:
+        login_data = json.load(login)
+
+    user = login_data["user"]
+    password = login_data["password"]
+    # to_address = 'codelabor1@gmail.com'
+    #
+    # body = "\r\n".join([
+    #   "Why, oh why",
+    #   "Why, oh why",
+    #   "Why, oh why"
+    #   ])
 
     @classmethod
     def email_send(cls):
@@ -20,21 +25,23 @@ class EmailSender:
         to_address = cls.to_address
         password = cls.password
         body = cls.body
+
         message = MIMEMultipart()
         message['From'] = user
         message['To'] = to_address
         message['Subject'] = "Welcome"
-        body = cls.body
         message.attach(MIMEText(body, 'plain'))
+
         try:
             server = smtplib.SMTP('smtp.gmail.com', 587)
             server.ehlo()
             server.starttls()
             server.login(user, password)
             server.sendmail(user, to_address, message.as_string())
-            print("Send")
+            print("Email sent")
             server.quit()
+
         except:
             print("Sending failed!")
 
-EmailSender.email_send()
+# EmailSender.email_send()
