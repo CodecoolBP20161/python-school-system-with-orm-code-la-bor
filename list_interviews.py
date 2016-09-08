ae55d1fc6c16d27a992424fb20c40a7d49768ee6
berloc <berloc1987@gmail.com>
2016-09-08 20:59:27 +0200

    mentor filter

--- list_interviews.py
+++ list_interviews.py
@@ -31,8 +31,8 @@ def get_rows_by_school_id(query):
 @list_interviews.route('/list/app_code', methods=['GET', 'POST'])
 def list_interview_app_code():
     if request.method == 'POST':
-        school_name = request.form['app_code']
-        rows = get_applicant_by_app_code(school_name)
+        app_code = request.form['app_code']
+        rows = get_applicant_by_app_code(app_code)
     else:
         rows = get_applicant_by_app_code(0)
     return render_template('list.html', applicants=rows)
@@ -45,31 +45,23 @@ def get_applicant_by_app_code(query):
         return Applicant.select().join(Interview).where(Applicant.app_code == query)
 
 
+@list_interviews.route('/list/mentor', methods=['GET', 'POST'])
+def list_interview_mentor():
+    if request.method == 'POST':
+        mentor = request.form['mentor']
+        rows = get_applicant_by_mentor(mentor)
+    else:
+        rows = get_applicant_by_mentor(0)
+    return render_template('list.html', applicants=rows)
+
+
+def get_applicant_by_mentor(query):
+    if query == "":
+        return Applicant.select().join(Interview).join(Mentor)
+    else:
+        return Applicant.select().join(Interview).join(Mentor).where(Mentor.last_name == query)
 
 
 @list_interviews.route('/filter')
 def get_filter():
     return render_template('filter.html')
-# @list_interviews.route('/list')
-# def filter_interviews_by_school():
-#     print(school_name)
-#     applicants = Applicant.select().join(Interview).where(Applicant.school == 1)
-#     return render_template('list.html', applicants=applicants)
-
-#
-# @list_interviews.route('/list/app_code')
-# def filter_interviews_by_school(app_code):
-#     query = Interview.select().where(Applicant.app_code == app_code)
-#     return render_template('list.html', query=query)
-#
-#
-# @list_interviews.route('/list/mentor')
-# def filter_interviews_by_school(mentor):
-#     query = Interview.select().where(mentor == mentor)
-#     return render_template('list.html', query=query)
-#
-#
-# @list_interviews.route('/list/reg_date')
-# def filter_interviews_by_school(reg_date):
-#     query = Interview.select().where(reg_date == reg_date)
-#     return render_template('list.html', query=query)
