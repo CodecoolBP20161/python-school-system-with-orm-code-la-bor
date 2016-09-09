3b00efd7c2d6720f0b0242f89cfc32bbd4feb1db
berloc <berloc1987@gmail.com>
2016-09-09 07:59:03 +0200

    fixing my fucked up

--- list_interviews.py
+++ list_interviews.py
@@ -5,7 +5,7 @@ from models import *
 list_interviews = Blueprint('list_interviews', __name__)
 
 
-@list_interviews.route('/list_interviews', methods=['GET', 'POST'])
+@list_interviews.route('/list_interviews')
 def list_interview():
     rows = Applicant.select().join(Interview)
     return render_template('list_interview.html', applicants=rows)
@@ -39,7 +39,7 @@ def list_interview_app_code():
 
 
 def get_applicant_by_app_code(query):
-    if query == "":
+    if query == "0":
         return Applicant.select().join(Interview)
     else:
         return Applicant.select().join(Interview).where(Applicant.app_code.contains(query))
